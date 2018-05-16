f9c79cc666305d78ee6c3ae314b90fcc7479386c
alecling <alecling@umail.iu.edu>
2018-05-16 09:18:16 -0400

    init commit
    this is the c

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG ={
+    apiKey: "AIzaSyAySx5pW1Bw09QNnKDVGvw4dP8mCyRaJyU",
+    authDomain: "fir-d2006.firebaseapp.com",
+    databaseURL: "https://fir-d2006.firebaseio.com",
+    projectId: "fir-d2006",
+    storageBucket: "fir-d2006.appspot.com",
+    messagingSenderId: "750618628442"
+}
